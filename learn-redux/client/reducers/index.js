51e9276918410cf0a9121769a03978e8648b6468
jerrysummer <reason2be@gmail.com>
2017-11-28 02:45:08 -0800

    reducers complete

---
+++ learn-redux/client/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'Redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({posts, comments, router: routerReducer});
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,8 @@
+// two parameters for reducers, 1 action 2 copy of current state;
+
+function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
+
+export default posts;
